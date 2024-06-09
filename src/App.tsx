e040318e3534a90fa0a4105c22e948db6f835bd9
Nextoore <anex831@gmail.com>
2024-06-09 20:48:10 +0300

    Anime web service

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div className="header">
+          <a href=""></a>
+          <div className="rightContainer">
+            <a href="" className="text-link">MOST POPULAR</a>
+            <a href="" className="text-link">MY COLLECTION</a>
+            <div className="searchBar"><input type='text' placeholder='Search anime'></input></div>
+          </div>
+        </div>
+        <div className="wrapper">
+
+        </div>
+        <div className="heare2">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
